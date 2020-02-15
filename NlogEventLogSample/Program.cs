b5a2ba43030aff919f7ed532c7cad39e61fa3d2e
後藤 祥太 <goto2536@toukei.co.jp>
2020-02-15 16:38:13 +0900

    新規作成

---
+++ NlogEventLogSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+
+namespace NlogEventLogSample
+{
+    internal static class Program
+    {
+        private static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                Logger.Info("Hello World");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+            }
+        }
+    }
+}
